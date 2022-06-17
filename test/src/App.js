d69580a85dff68273dc06605b06e70eb54825aa3
Irina <irenserge@mail.ru>
2022-06-17 18:30:38 +0300

    feat: add header

--- test/src/App.js
+++ test/src/App.js
@@ -5,21 +5,29 @@ import TechPage from './Pages/TechPage/TechPage';
 import ClothesPage from './Pages/ClothesPage/ClothesPage';
 import AllPage from './Pages/AllPage/AllPage';
 import Layout from './components/Layout/Layout';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import './App.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 class App extends React.Component {
   
   render() {
     return (
       <>
+     <ApolloProvider client={client}>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<AllPage />} />
-          <Route path="tech" element={<TechPage />} />
-          <Route path="clothes" element={<ClothesPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<AllPage />} />
+            <Route path="tech" element={<TechPage />} />
+            <Route path="clothes" element={<ClothesPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </ApolloProvider>
       </>
     );
   }
--- test/src/components/Header/Header.jsx
+++ test/src/components/Header/Header.jsx
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { gql, useQuery } from "@apollo/client";
 import { NavLink } from 'react-router-dom';
 import * as Styled from './Header.style';
+import cart from '../../assets/Vector.svg';
 
-class Header extends React.Component {
-
-    render() {
-        return(
-            <>
-            <Styled.Wrapper>
-                <Styled.Ul>
-                    <NavLink className={({ isActive }) => (isActive ? 'active-link' : '')} to={'/'}>All</NavLink>
-                </Styled.Ul>
-                <Styled.Ul>
-                    <NavLink className={({ isActive }) => (isActive ? 'active-link' : '')} to={'tech'}>Tech</NavLink>  
-                </Styled.Ul>
-                <Styled.Ul>
-                    <NavLink className={({ isActive }) => (isActive ? 'active-link' : '')} to={'clothes'}>Clothes</NavLink>
-                </Styled.Ul>
-            </Styled.Wrapper>
-            </>
-        )
+const FETCH_DATA = gql`
+    query {
+        currencies {
+            symbol
+        }
     }
+`;
+
+const Header = () => {
+    const {data, loading} = useQuery(FETCH_DATA);
+    const [currency, setCurrency] = useState([]);
+    useEffect(() => {
+        if(!loading) {
+            setCurrency(data.currencies)
+        }
+    }, [data, loading])
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    return(
+        <>
+        <Styled.Wrapper>
+            <Styled.Ul>
+                <NavLink className={({ isActive }) => (isActive ? 'active-link' : '')} to={'/'}>All</NavLink>
+            </Styled.Ul>
+            <Styled.Ul>
+                <NavLink className={({ isActive }) => (isActive ? 'active-link' : '')} to={'tech'}>Tech</NavLink>  
+            </Styled.Ul>
+            <Styled.Ul>
+                <NavLink className={({ isActive }) => (isActive ? 'active-link' : '')} to={'clothes'}>Clothes</NavLink>
+            </Styled.Ul>
+            <select>
+                {currency.map(item => <option key={item.symbol}>{item.symbol}</option>)}
+            </select>
+            <img src={cart} alt="cart" />
+        </Styled.Wrapper>
+        </>
+    )
 }
 
 export default Header;
